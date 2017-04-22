e3a9c7647363a64902e4122d95f7e1ddf20e06be
Ericmalcangi <eric.malcangi@yahoo.com.ar>
2017-04-22 16:09:46 -0300

    primer commit

---
+++ Tp1-Disco/Test/ar/edu/unlam/porgramacionbasica2/Test.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.porgramacionbasica2;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
